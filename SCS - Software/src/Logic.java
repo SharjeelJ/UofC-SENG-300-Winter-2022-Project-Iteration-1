9c2f9cce7426e76d0b24d423bb983fe8020393cf
Sharjeel Junaid <sharjeelj@outlook.com>
2022-03-13 19:43:57 -0600

    Added Eclipse Project files

--- SCS - Software/src/Logic.java
+++ SCS - Software/src/Logic.java
@@ -1,7 +1,10 @@
+import org.lsmr.selfcheckout.devices.ElectronicScale;
+
 public class Logic
 {
-    public static void main(String[] args)
+	public static void main(String[] args)
     {
+		ElectronicScale test;
         System.out.println("Hello World");
     }
 }
