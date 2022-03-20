1f3e42f23270d03f59b1f23b1cd9dfea34b98cf7
Sharjeel Junaid <sharjeelj@outlook.com>
2022-03-20 16:30:09 -0600

    Changed the scan item use case to store all the barcodes scanned

--- SCS - Software/src/ScanItem.java
+++ SCS - Software/src/ScanItem.java
@@ -1,46 +1,46 @@
-// imports
-import java.util.List;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
 import org.lsmr.selfcheckout.Barcode;
-import org.lsmr.selfcheckout.devices.BarcodeScanner;
-import org.lsmr.selfcheckout.devices.observers.BarcodeScannerObserver;
 import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.BarcodeScanner;
 import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
-import org.lsmr.selfcheckout.devices.*;
-import org.lsmr.selfcheckout.devices.observers.*;
+import org.lsmr.selfcheckout.devices.observers.BarcodeScannerObserver;
 
+import java.util.ArrayList;
 
-// potential returns
-// return totalCost
-// return cartWeight
+public class ScanItem implements BarcodeScannerObserver
+{
+    public ArrayList <Barcode> barcodesScanned = new ArrayList <>();
 
-
-public class ScanItem {
-    private SelfCheckoutStation checkoutStation;
-    private Double cartWeight;
-    private BigDecimal totalCost;   // amount to be paid/ total
-
-    public ScanItem(//parameters for this constructor)
+    /**
+     * Announces that the indicated device has been enabled.
+     *
+     * @param device The device that has been enabled.
+     */
+    @Override
+    public void enabled(AbstractDevice <? extends AbstractDeviceObserver> device)
     {
 
     }
-    
-    // implementing observer
-    private class SI implements BarcodeScannerObserver {
-        @Override
-        public void enabled(AbstractDevice<? extends AbstractDeviceObserver>device)
-        {
 
-        }
-        
-        public void disabled(AbstractDevice<? extends AbstractDeviceObserver>device)
-        {
+    /**
+     * Announces that the indicated device has been disabled.
+     *
+     * @param device The device that has been enabled.
+     */
+    @Override
+    public void disabled(AbstractDevice <? extends AbstractDeviceObserver> device)
+    {
 
-        }
     }
-     
+
+    /**
+     * An event announcing that the indicated barcode has been successfully scanned.
+     *
+     * @param barcodeScanner The device on which the event occurred.
+     * @param barcode
+     */
+    @Override
+    public void barcodeScanned(BarcodeScanner barcodeScanner, Barcode barcode)
+    {
+        barcodesScanned.add(barcode);
+    }
 }
